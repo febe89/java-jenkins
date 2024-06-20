d7cfa6934a15b27f3fa4bd97ee5d292131923363
febe89 <fullstack9020@gmail.com>
2024-06-20 23:01:59 +0530

    added student class

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ class Main{
   public static void main(String[] args){
     System.out.println("This is java program");
     System.out.println(new Date());
-    System.out.println(2+2);
+    System.out.println(2 + 2);
+    Student student = new Student();
+   student.sayHi();
   }
  }
